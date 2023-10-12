32a47769144ab0d060ceed2293696e99b227143c
FARIDRIZQY <faridivanrizqy501@gmail.com>
2023-10-12 23:46:48 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,10 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p> ======================== </p>
+    <p> Nama  : Farid Ivan Rizqy </p>
+    <p> NIM   : 19220624         </p>
+    <p> kelas : 19.3A.04         </p>
+    <p> Hobi  : badminton        </p>
+    <p> ======================== </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eos, doloremque consequuntur error a recusandae laudantium veniam, nemo excepturi quae, quod eligendi? Illo, est quo laboriosam sint corporis cum mollitia. Similique molestiae rerum consequuntur est quibusdam eum minima ut voluptatem porro laudantium illo natus, cumque tenetur accusantium dicta commodi neque?</p>
+</section>
